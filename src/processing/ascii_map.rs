2b7510af2ff4f809e9b36a28f1f8f7005ff48a4f
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-12-22 22:58:49 +0800

    Rewrite

--- src/processing/ascii_map.rs
+++ src/processing/ascii_map.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn ascii_normal() -> HashMap<&'static str, &'static str> {
+pub fn ascii_normal<'a, 'b>() -> HashMap<&'a str, &'b str> {
     HashMap::<&str, &str>::from([
         (
             "hakurei_reimu",
@@ -34,6 +34,31 @@ pub fn ascii_normal() -> HashMap<&'static str, &'static str> {
      \-_-/            \      /
                        \-__-/"#,
         ),
-        ("", ""),
+        ("kirisame_marisa", ""),
     ])
+}
+
+#[cfg(test)]
+mod fumo_ascii_tests {
+    use crate::processing::DISTRO_NORMAL_MAX_SIZE;
+
+    use super::*;
+
+    #[test]
+    fn test_ascii_normal() {
+        ascii_normal().values().for_each(|fumo| {
+            fumo.lines()
+                .for_each(|line| assert!(line.len() <= DISTRO_NORMAL_MAX_SIZE))
+        })
+    }
+
+    #[test]
+    fn test_ascii_small() {
+        // TODO: implement small size test.
+    }
+
+    #[test]
+    fn test_ascii_large() {
+        // TODO: implement large size test.
+    }
 }
